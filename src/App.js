f5df288bb410948d5b1664d5ad1448b47f8d8d12
yutakobayashidev <yutakobayashi0924@gmail.com>
2022-07-21 19:09:32 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={this.handleClick} className="Button">
+          New Page
+        </button>
       </header>
     </div>
-  );
+    );
+  }
+  // ***** クリック時呼び出される関数を追加 *****
+  handleClick() {
+    
+    const obj = {parent: {
+      "type": "database_id",
+      "database_id": process.env.REACT_APP_NOTION_DATABASE
+    },
+    "properties": {
+      "Name": {
+          "title": [
+              {
+                  "text": {
+                      "content": "ウェブからのページ追加"
+                  }
+              }
+          ]
+      },
+    }};
+
+
+    const body = JSON.stringify(obj);
+    
+    const headers = {
+      'Content-Type': 'application/json',
+      'Notion-Version': '2021-05-13',
+      'Authorization': "Bearer " + process.env.REACT_APP_NOTION_TOKEN
+    };
+    
+
+    const method = "POST";
+
+    fetch("https://notion.yutakobayashi.workers.dev/https://api.notion.com/v1/pages", {method, headers,body}).then((res)=> res.json()).then(console.log).catch(console.error);
+
+  }
+  // ***** ここまで追加 *****
 }
 
 export default App;
